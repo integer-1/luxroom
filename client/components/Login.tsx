d1ade19963eb0997fc4b4cd1264cf5ddde12e7f3
Suwon Jeong <integer.won@gmail.com>
2023-12-15 12:02:04 +1300

    change login, logouta and cart icon and add overy trigger

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,26 +1,39 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
-import { CartIcon } from './Icon'
+import { CartIcon, LoginIcon, LogoutIcon } from './Icon'
 import { Link } from 'react-router-dom'
+import { Tooltip } from 'react-bootstrap'
+import { OverlayTrigger } from 'react-bootstrap'
 
 const Login = () => {
-  const { loginWithRedirect, logout, user } = useAuth0()
-  
+  const { loginWithRedirect, logout } = useAuth0()
+
   return (
     <div id="login">
       <IfAuthenticated>
-        <p>{user?.nickname}</p>
-        <Link to="/my/cart">
-          <CartIcon />
-        </Link>
-        <button className="button" onClick={() => logout()}>
-          Logout
-        </button>
+        <OverlayTrigger
+          overlay={<Tooltip id="tooltip-disabled">Logout</Tooltip>}
+        >
+          <button className="logout-button" onClick={() => logout()}>
+            <LogoutIcon />
+          </button>
+        </OverlayTrigger>
+        <OverlayTrigger
+          overlay={<Tooltip id="tooltip-disabled">My Cart</Tooltip>}
+        >
+          <Link to="/my/cart">
+            <CartIcon />
+          </Link>
+        </OverlayTrigger>
       </IfAuthenticated>
       <IfNotAuthenticated>
-        <button className="button" onClick={() => loginWithRedirect()}>
-          Login
-        </button>
+        <OverlayTrigger
+          overlay={<Tooltip id="tooltip-disabled">Login</Tooltip>}
+        >
+          <button className="login-button" onClick={() => loginWithRedirect()}>
+            <LoginIcon />
+          </button>
+        </OverlayTrigger>
       </IfNotAuthenticated>
     </div>
   )
