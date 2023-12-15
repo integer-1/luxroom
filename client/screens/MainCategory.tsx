8798c20495a41190c4d731f374da04c641b0f198
Suwon Jeong <integer.won@gmail.com>
2023-12-15 14:51:14 +1300

    add css style  for main category list

--- client/screens/MainCategory.tsx
+++ client/screens/MainCategory.tsx
@@ -3,7 +3,6 @@ import { Link, useParams } from 'react-router-dom'
 import { getChairByMain } from '../apis/chairs'
 import { Chair } from '../../models/chairs'
 import SubLink from '../components/SubLink'
-import { Container, Row, Col } from 'react-bootstrap'
 import LoadingPage from '../components/LoadingPage'
 
 const MainCategory = () => {
@@ -24,7 +23,7 @@ const MainCategory = () => {
 
   if (isError) return <p>Something went wrong!</p>
 
-  if (!chairs || isLoading) return <LoadingPage/>
+  if (!chairs || isLoading) return <LoadingPage />
 
   return (
     <div className="main">
@@ -34,26 +33,20 @@ const MainCategory = () => {
         <SubLink mainCategory={mainCategory} />
       </div>
 
-      <div className="main-category">
-        <Container>
-          <Row>
-            {chairs.map((chair: Chair, index: number) => (
-              <Col sm={12} md={6} lg={3} key={index}>
-                <div className="main-category-list" key={chair.code}>
-                  <Link
-                    to={`/${chair.mainCategory}/${chair.subCategory}/${chair.name}`}
-                    state={chair}
-                    >
-                    <img src={`../../${chair.code}.jpg`} alt={chair.name} />
-                    <p className="main-category-list">{chair.name}</p>
-                  </Link>
-                  <p>{chair.subCategory} Chair</p>
-                  <p>$ {chair.price}</p>
-                </div>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+      <div className="main-category-list">
+        {chairs.map((chair: Chair) => (
+          <div className="main-category-list-box" key={chair.code}>
+            <Link
+              to={`/${chair.mainCategory}/${chair.subCategory}/${chair.name}`}
+              state={chair}
+            >
+              <img src={`../../${chair.code}.jpg`} alt={chair.name} />
+              <p className="main-category-list-name">{chair.name.replace(/-/g, ' ')}</p>
+            </Link>
+            <p className="main-category-list-sub">{chair.subCategory} Chair</p>
+            <p className="main-category-list-price">$ {chair.price}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
